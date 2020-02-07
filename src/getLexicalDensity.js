21a839b784762370c813492cbb776a6aea92a77d
jagadeeshdumpeta <44097695+jagadeeshdumpeta@users.noreply.github.com>
2020-02-07 13:14:21 +0100

    Added new route to add non lexial words
    Added new route to add non lexial words

--- src/getLexicalDensity.js
+++ src/getLexicalDensity.js
@@ -4,7 +4,7 @@ const url =  require('url');
 const app = express();
 app.use(express.json());
 
-
+//Route to get lexical density of given input which internally calls API.
 app.get('/complexity', (req, res) => {
     const data = fs.readFileSync('./NonLexicalWords.txt', 'utf8');
     const nonLexicalWords = data.split(",");
@@ -12,6 +12,7 @@ app.get('/complexity', (req, res) => {
     res.send(result);
 });
 
+//API to calculate lexical density of  given input according to the route and non lexical words provided.
 function getLexicalDensity (inputText, reqUrl, nonLexicalWords) {
     const totalWords = inputText.split(" "); 
     if(totalWords.length <= 100 && inputText.length <= 1000){
@@ -43,6 +44,15 @@ app.listen(3000, () =>{
     console.log(`Listening on port 3000..`)
 });
 
+//Route to add new non lexical words to file NonLexicalWords.txt.
+app.get('/addNonLexicalWord',(req, res) => {
+    fs.appendFile('./NonLexicalWords.txt', ',newWord', function (err) {
+        if (err) throw err;
+        console.log('Updated!');
+      });
+});
+
+//exporting API.
 module.exports = function getLexicalDensityAPI(inputText, reqUrl, nonLexicalWords){    
     return result = getLexicalDensity(inputText, reqUrl, nonLexicalWords);
     console.log(result);
